1adffbb7641bfa264b179370a9c037d0a65577cc
antonellalalli <lalliantonellacl@gmail.com>
2026-05-26 12:27:47 -0300

    Ajustes

--- utils/api.js
+++ utils/api.js
@@ -4,8 +4,5 @@ const {VITE_API_URL} =import.meta.env;
 
 export const api = axios.create({
     baseURL: `https://jornadasmetalurgia2026-back.onrender.com/api`,
-    headers: {
-        "Content-Type": "application/json"
-    },
     withCredentials: true
 })
