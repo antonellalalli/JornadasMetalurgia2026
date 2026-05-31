cd5ca745d4634eb8e80f395a4aba7cc24825e1d5
antonellalalli <lalliantonellacl@gmail.com>
2026-05-31 18:35:13 -0300

    boton enviar

--- src/components/Inscription.jsx
+++ src/components/Inscription.jsx
@@ -288,7 +288,8 @@ export default function Inscription() {
           </div>
           <button
             type="submit"
-            className=" text-white font-bold   text-[20px] xl:text-[24px] rounded-2xl w-50 xl:w-150 h-14 m-auto mt-3 mb-5 bg-orange-500 hover:bg-orange-600 focus:ring-orange-500 focus:ring-offset-2 cursor-pointer ">
+            disabled={loading}
+            className=" text-white font-bold   text-[20px] xl:text-[24px] rounded-2xl w-50 xl:w-150 h-14 m-auto mt-3 mb-5 bg-orange-500 hover:bg-orange-600 focus:ring-orange-500 focus:ring-offset-2 cursor-pointer disbled:cursor-default">
             {loading ? "INSCRIBIENDOSE..." : "INSCRIBIRSE"}
           </button>
           </form>
